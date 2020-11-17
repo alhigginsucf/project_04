8d73d811d555df3e4b4612896df5008867554ce6
Alexander Higgins <65186976+alhigginsucf@users.noreply.github.com>
2020-11-17 14:44:19 -0500

    updated project settings.py and urls.py. Created index.html and ticker.html.

---
+++ stocks/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import TickerForm
+
+
+def index(request): 
+    if request.method == 'POST':
+        form = TickerForm(request.POST)
+        if form.is_valid():
+            ticker = request.POST['ticker']
+            return HttpResponseRedirect(ticker)
+    else:
+        form = TickerForm()
+    return render(request, 'index.html', {'form': form})
+
+def ticker(request, tid):
+    context = {}
+    context['ticker'] = tid
+    return render(request, 'ticker.html', context)
